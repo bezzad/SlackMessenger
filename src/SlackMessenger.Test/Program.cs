43e2d4cb56ae86d8eba45254021bc7f4be0f5833
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2018-05-06 16:38:31 +0430

    Update Program.cs

--- src/SlackMessenger.Test/Program.cs
+++ src/SlackMessenger.Test/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            var client = new SlackClient("https://hooks.slack.com/services/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");
+            var client = new SlackClient("https://hooks.slack.com/services/Your/WebHook/Url");
             var msg = new Message($"Test Message for slack channel", "channel name", "username", "https://a.slack-edge.com/9c217/img/loading_hash_animation.gif");
             client.Send(msg);
         }
